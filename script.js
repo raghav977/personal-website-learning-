51c660d69033604c575630112d68b09157b3e278
Raghav Dahal <88540324+raghav977@users.noreply.github.com>
2024-06-06 00:37:29 +0545

    personal website learning
    learning to build some websites

---
+++ script.js
@@ -0,0 +1,15 @@
+function sendemail(){
+    window.location = "mailto:raghavdahal686@gmail.com"
+}
+function websitedemo(){
+    window.location = "https://raghav977.github.io/ecommerce/"
+}
+function websitedemo1(){
+    window.location = "https://raghav977.github.io/radhikains/"
+}
+function websitedemo3(){
+    window.location = "https://github.com/raghav977/Encrypt-and-decrypt-program"
+}
+function downloadcv(){
+    
+}
